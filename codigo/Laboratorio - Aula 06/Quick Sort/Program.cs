edcb4c6df370f78f31559abde4c0b944f7082f32
Pedro Siqueira <97108963+pedrosiqueirapb@users.noreply.github.com>
2022-10-10 15:14:01 -0300

    Merge Sort, Quick Sort e Shell Sort

---
+++ codigo/Laboratorio - Aula 06/Quick Sort/Program.cs
@@ -0,0 +1,35 @@
+﻿void QuickSort(int[] Vetor, int Inicio, int Fim){
+    int i = Inicio, j = Fim, pivo = Vetor[Inicio];
+
+    while(i <= j){
+        while(Vetor[i] < pivo){
+            i++;
+        }
+        while(Vetor[j] > pivo){
+            j--;
+        }
+        if(i <= j){
+            int aux = Vetor[i];
+            Vetor[i] = Vetor[j];
+            Vetor[j] = aux;
+            i++;
+            j--;
+        }
+    }
+    if(Inicio < j){
+        QuickSort(Vetor, Inicio, j);
+    }
+    if(i < Fim){
+        QuickSort(Vetor, i, Fim);
+    }
+}
+
+int[] vet = new int[10];
+Random numAleatorio = new Random();
+for(int i=0; i<vet.Length; i++){
+    vet[i] = numAleatorio.Next(0,50);
+}
+QuickSort(vet, 0, vet.Length-1);
+for(int j=0; j<vet.Length-1; j++){
+    Console.Write(vet[j] + " ");
+}
---
+++ codigo/Laboratorio - Aula 06/Shell Sort/Program.cs
@@ -0,0 +1,24 @@
+﻿void ShellSort(int[]Vetor, int Tam){
+    for (int h=Tam/2; h>0; h/=2){
+        for (int i = h; i < Tam; i++){
+            int valor = Vetor[i];
+            int k = i;
+
+            while (k>=h && Vetor[k-h] > valor){
+                Vetor[k] = Vetor[k-h];
+                k -= h;
+            }
+            Vetor[k] = valor;
+        }
+    }
+}
+
+int[] vet = new int[10];
+Random numAleatorio = new Random();
+for(int i=0; i<vet.Length; i++){
+    vet[i] = numAleatorio.Next(0,100);
+}
+ShellSort(vet, vet.Length);
+for(int j=0; j<vet.Length; j++){
+    Console.Write(vet[j] + " ");
+}
