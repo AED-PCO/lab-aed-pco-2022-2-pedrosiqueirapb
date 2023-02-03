e2c4ce6258fb28ce7237971a25e33e8889c3232d
Pedro Siqueira <97108963+pedrosiqueirapb@users.noreply.github.com>
2023-02-03 09:36:18 -0300

    Removendo contadores

--- codigo/Laboratorio - Aula 06/Bubble Sort/Program.cs
+++ codigo/Laboratorio - Aula 06/Bubble Sort/Program.cs
@@ -1,9 +1,7 @@
 ﻿/* 10, 100, 500, 1000, 5000, 10000 */
-/* comparação e troca */
 
 Random numAleatorio = new Random();
 
-int comp=0, trc=0;
 int[] vet = new int [10];
 for(int j=0; j<vet.Length; j++){
     vet[j] = numAleatorio.Next(0,100); 
@@ -17,9 +15,7 @@ void BubbleSort (int[] Vetor){
                 aux = Vetor[j];
                 Vetor[j] = Vetor[j+1];
                 Vetor[j+1] = aux;
-                trc++;
             }
-            comp++;
         }
     }
 }
@@ -29,4 +25,3 @@ BubbleSort(vet);
 for(int i=0; i<vet.Length; i++){
     Console.Write(vet[i] + " ");
 }
-Console.WriteLine("\nComparações = {0}, Trocas = {1}", comp, trc);
